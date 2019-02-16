c1f27812a96ef1ed1a7ecbdacaec1eb39dfc1e7a
Ferenc Nyerges <ferenc.nyerges@intland.com>
2019-02-16 12:00:53 +0100

    Adding project skeleton

---
+++ src/main/java/sorts/App.java
@@ -0,0 +1,13 @@
+package sorts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello beautiful World!" );
+    }
+}
